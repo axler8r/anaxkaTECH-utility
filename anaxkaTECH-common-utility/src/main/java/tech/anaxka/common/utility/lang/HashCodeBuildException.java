c0071dee0fc0a7a40ab4d753839f2acd4849fdd9
Axl Mattheus <axl.mattheus@gmail.com>
2014-03-01 20:04:32 +0200

    Added new Exceptions for modified functor API.

---
+++ anaxkaTECH-common-utility/src/main/java/tech/anaxka/common/utility/lang/HashCodeBuildException.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright (c) 2014, 4axka (Pty) Ltd
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package tech.anaxka.common.utility.lang;
+
+
+import tech.anaxka.common.utility.exception.NonFatalException;
+
+
+
+/**
+ *
+ * @author <a href="mailto:info@anaxka.tech?Subject=RFI">anaxkaTECH (Pty) Ltd</a>
+ */
+public class HashCodeBuildException extends NonFatalException {
+    private static final long serialVersionUID = 5463530114329668733L;
+
+    /** {@inheritDoc} */
+    public HashCodeBuildException() {
+    }
+
+    /** {@inheritDoc}
+     * @param message */
+    public HashCodeBuildException(final String message) {
+        super(message);
+    }
+
+    /** {@inheritDoc}
+     * @param message
+     * @param cause */
+    public HashCodeBuildException(final String message, final Throwable cause) {
+        super(message, cause);
+    }
+
+    /** {@inheritDoc}
+     * @param cause */
+    public HashCodeBuildException(final Throwable cause) {
+        super(cause);
+    }
+
+    /** {@inheritDoc}
+     * @param message
+     * @param cause
+     * @param enableSuppression
+     * @param writableStackTrace */
+    public HashCodeBuildException(
+            final String message,
+            final Throwable cause,
+            final boolean enableSuppression,
+            final boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
